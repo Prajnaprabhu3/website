0aa22c3e4a44f382486d7139aa97a3fcad9b5eb1
Reviewpad team <adriano@reviewpad.com>
2022-10-12 11:50:10 +0100

    refactor: update website name and description

--- env.ts
+++ env.ts
@@ -1,6 +1,7 @@
 export const EnvVars = {
-  SITE_NAME: 'My SaaS Startup',
-  OG_IMAGES_URL: 'https://next-saas-starter-ashy.vercel.app/og-images/',
-  URL: 'https://next-saas-starter-ashy.vercel.app/',
-  MAILCHIMP_SUBSCRIBE_URL: 'https://bstefanski.us5.list-manage.com/subscribe/post?u=66b4c22d5c726ae22da1dcb2e&id=679fb0eec9',
+  SITE_NAME: 'Reviewpad',
+  OG_IMAGES_URL: 'https://website-reviewpad.vercel.app/og-images/',
+  URL: 'https://website-reviewpad.vercel.app/',
+  MAILCHIMP_SUBSCRIBE_URL: 'https://reviewpad.us20.list-manage.com/subscribe?u=d3192386c1072f6b5fd28c305&id=861e1274f6',
+  SITE_DESCRIPTION: 'Pull Request Automation'
 };
