d167e05e25644d512a5d4d073adde253fa07e757
Reviewpad team <adriano@reviewpad.com>
2022-10-12 14:29:11 +0100

    refactor: update navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,10 +20,10 @@ import { NewsletterModalContextProvider, useNewsletterModalContext } from 'conte
 import { NavItems } from 'types';
 
 const navItems: NavItems = [
-  { title: 'Awesome SaaS Features', href: '/features' },
-  { title: 'Pricing', href: '/pricing' },
-  { title: 'Contact', href: '/contact' },
-  { title: 'Sign up', href: '/sign-up', outlined: true },
+  { title: 'Documentation', href: 'https://docs.reviewpad.com' },
+  { title: 'About Us', href: '/about-us' },
+  { title: 'Blog', href: '/blog' },
+  // { title: 'Sign up', href: '/sign-up', outlined: true },
 ];
 
 const TinaCMS = dynamic(() => import('tinacms'), { ssr: false });
