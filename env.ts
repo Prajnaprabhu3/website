950279d1f8b332696df1c8bad662cf412c9315a6
Reviewpad team <adriano@reviewpad.com>
2022-10-13 15:35:34 +0100

    Refactor: Update mailchimp subscribe url
    Include a field tag in the URL identifying where did the user subscribed from.

--- env.ts
+++ env.ts
@@ -2,6 +2,6 @@ export const EnvVars = {
   SITE_NAME: 'Reviewpad',
   OG_IMAGES_URL: 'https://website-reviewpad.vercel.app/og-images/',
   URL: 'https://website-reviewpad.vercel.app/',
-  MAILCHIMP_SUBSCRIBE_URL: 'https://reviewpad.us20.list-manage.com/subscribe?u=d3192386c1072f6b5fd28c305&id=861e1274f6',
+  MAILCHIMP_SUBSCRIBE_URL: 'https://reviewpad.us20.list-manage.com/subscribe/post-json?u=d3192386c1072f6b5fd28c305&id=861e1274f6&MMERGE3=Website',
   SITE_DESCRIPTION: 'Pull Request Automation'
 };
