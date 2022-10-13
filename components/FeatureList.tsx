3297a6613d92d3f7db80e4e3b372631b0cf8f133
Reviewpad team <adriano@reviewpad.com>
2022-10-13 12:01:30 +0100

    feat: Feature list

---
+++ components/FeatureList.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import styled from 'styled-components';
+import { media } from 'utils/media';
+import Container from './Container';
+import RichText from './RichText';
+import Link from 'components/Link';
+
+export default function FeatureList() {
+  return (
+    <>
+      <FeatureListWrapper>
+        <ContentContainerLeft>  
+          <Title>Reviewer Assignment</Title>
+          <RichText>Specify ownership at the semantic level and also based on the nature of the changes. Import central configuration files. Define which rules have precedence.</RichText>
+          <FeatureLink><Link href="https://docs.reviewpad.com/use-cases/reviewer-assignment">Read more ↗︎</Link></FeatureLink>
+        </ContentContainerLeft>
+        <ContentContainer>
+          <Title>Automated Labelling</Title>
+          <RichText>Create or update labels based in file paths, author groups, missing changes, code patterns, semantic code annotations, etc.</RichText>
+          <FeatureLink><Link href="https://docs.reviewpad.com/use-cases/automated-labelling">Read more ↗︎</Link></FeatureLink>
+        </ContentContainer>
+      </FeatureListWrapper>
+      <FeatureListWrapper>
+        <ContentContainerLeft>  
+          <Title>Enforce Conventions</Title>
+          <RichText>Enforce branch and commit naming policies, and also automatically add labels based on the branch names. Check and provide error messages if commits do not comply.</RichText>
+          <FeatureLink><Link href="https://docs.reviewpad.com/use-cases/enforce-branch-conventions">Read more ↗︎</Link></FeatureLink>
+        </ContentContainerLeft>
+        <ContentContainer>
+          <Title>Automerge</Title>
+          <RichText>Using the semantic features of Reviewpad, configure scenarios where auto-merge seems natural. Protect sensitive code with critical zones (dynamic or static generated).</RichText>
+          <FeatureLink><Link href="https://docs.reviewpad.com/use-cases/auto-merge">Read more ↗︎</Link></FeatureLink>
+        </ContentContainer>
+      </FeatureListWrapper>
+    </>
+  );
+}
+
+const Title = styled.h1`
+  font-size: 5.2rem;
+  font-weight: bold;
+  line-height: 1.1;
+  margin-bottom: 4rem;
+  letter-spacing: -0.03em;
+
+  ${media('<=tablet')} {
+    font-size: 4.6rem;
+    margin-bottom: 2rem;
+  }
+`;
+
+const FeatureLink = styled.div`
+  font-size: 1.8rem;
+  font-weight: bold;
+  opacity: 0.8;
+  line-height: 3.6;
+`;
+
+const ContentContainerLeft = styled.div`
+  flex: 1;
+
+  ${media('<=desktop')} {
+    width: 100%;
+  }
+`;
+
+const ContentContainer = styled.div`
+  flex: 1;
+`;
+
+const FeatureListWrapper = styled(Container)`
+  display: flex;
+  // align-items: center;
+
+  ${ContentContainerLeft} {
+    margin: 0 5rem 0 0;
+  }
+
+  ${media('<=desktop')} {
+    flex-direction: column;
+
+    ${ContentContainerLeft} {
+      margin: 0 0 2.5rem 0;
+    }
+  }
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ import Hero from 'views/HomePage/Hero';
 import Partners from 'views/HomePage/Partners';
 import ScrollableBlogPosts from 'views/HomePage/ScrollableBlogPosts';
 import Testimonials from 'views/HomePage/Testimonials';
+import FeatureList from 'components/FeatureList';
 
 export default function Homepage({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
@@ -25,26 +26,7 @@ export default function Homepage({ posts }: InferGetStaticPropsType<typeof getSt
         <WhiteBackgroundContainer>
           <Hero />
           <Partners />
-          <BasicSection imageUrl="/demo-illustration-1.svg" title="Lorem ipsum dolor sit amet consectetur." overTitle="sit amet gogo">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, quidem error incidunt a doloremque voluptatem porro inventore
-              voluptate quo deleniti animi laboriosam.{' '}
-              <Link href="/help-center">Possimus ullam velit rem itaque consectetur, in distinctio?</Link> Lorem ipsum, dolor sit amet
-              consectetur adipisicing elit. Soluta repellendus quia quos obcaecati nihil. Laudantium non accusantium, voluptate eum nesciunt
-              at suscipit quis est soluta?
-            </p>
-          </BasicSection>
-          <BasicSection imageUrl="/demo-illustration-2.svg" title="Lorem ipsum dolor sit." overTitle="lorem ipsum" reversed>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, quidem error incidunt a doloremque voluptatem porro inventore{' '}
-              <strong>voluptate quo deleniti animi laboriosam</strong>. Possimus ullam velit rem itaque consectetur, in distinctio?
-            </p>
-            <ul>
-              <li>Professional point 1</li>
-              <li>Professional remark 2</li>
-              <li>Professional feature 3</li>
-            </ul>
-          </BasicSection>
+          <FeatureList />
         </WhiteBackgroundContainer>
         <DarkerBackgroundContainer>
           <Cta />
