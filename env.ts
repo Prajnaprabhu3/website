71c222ce98357b09f9d551a6eb09b2f75d9115d1
Reviewpad team <adriano@reviewpad.com>
2022-10-17 15:40:04 +0100

    feat: Tina set up
    This is a platform to assist markdown blogging

--- env.ts
+++ env.ts
@@ -3,5 +3,7 @@ export const EnvVars = {
   OG_IMAGES_URL: 'https://website-reviewpad.vercel.app/og-images/',
   URL: 'https://website-reviewpad.vercel.app/',
   MAILCHIMP_SUBSCRIBE_URL: 'https://reviewpad.us20.list-manage.com/subscribe/post-json?u=d3192386c1072f6b5fd28c305&id=861e1274f6&MMERGE3=Website',
-  SITE_DESCRIPTION: 'Pull Request Automation'
+  SITE_DESCRIPTION: 'Pull Request Automation',
+  NEXT_PUBLIC_ORGANIZATION_NAME: 'Reviewpad',
+  NEXT_PUBLIC_TINA_CLIENT_ID: 'b61d2f02-cf9d-4ad6-ac16-6b2bd392f1f2'
 };
